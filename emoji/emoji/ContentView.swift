7b8eab0bf29764638507f2946d0ca3ddc1fba410
Ma-2005 <bo.sali7.q8@gmail.com>
2022-08-19 14:30:46 +0300

    ..

--- emoji/emoji/ContentView.swift
+++ emoji/emoji/ContentView.swift
@@ -8,28 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var Myemojy = 0
+    @State var Myemojy = ""
     @State var emojies = ["😂","😭","🫡","💙","😎","🔥","😅","🐥","🫀","🤝🏻","👏🏽","😡","🥺"]
 
     
     var body: some View {
         ZStack{
-            Image("22")
+            Image("11")
                 .resizable()
                 .ignoresSafeArea()
             
-            VStack(spacing: 80){
+            VStack(spacing: 100){
                 Text("Choose your favorite emoji")
                     .font(.custom("Amiri-Bold", size: 40))
-                    .foregroundColor(.white)
-                    .shadow(color: .blue, radius: 5)
+                    .foregroundColor(.black)
+                    .shadow(color: .green, radius: 5)
                     .padding()
-                    .background(.white.opacity(0.2))
+                    .background(.white.opacity(0.6))
                     .cornerRadius(20)
                 
                 
-                    
-                
+           Text(Myemojy)
+                    .font(.custom("Amiri-Bold", size: 130))
+                    .frame(width: 175, height: 175)
+                    .background(.white.opacity(0.75))
+                    .cornerRadius(150)
                 
                 ScrollView(.horizontal){
                     HStack(spacing: 25){
@@ -41,16 +44,16 @@ struct ContentView: View {
                                 .background(.white.opacity(0.5))
                                 .cornerRadius(400)
                                 .onTapGesture {
-                                    
-                                }
+                                    Myemojy = m
+                            }
                         }
-                      
                     }
                 }
             }
         }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
